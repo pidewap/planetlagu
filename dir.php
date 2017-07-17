456795650cbe4efa026c2dc8ceadd56912efb509
pidewap <pidewap@gmail.com>
2017-07-17 09:50:09 +0700

    Update dir.php

--- dir.php
+++ dir.php
@@ -1,7 +1,7 @@
 <?php
-if(empty($_GET['id'])){
+if(!empty($_GET['id'])){
 $url='http://admin.wapka.com/wap/en/1898520/cm/manager/view/content_type/2/id/'.$_GET['id'].'/tw_pr/xhtml';
-header("location: '.$url.'");
+header("location: $url");
 }
 echo '<form action="/dir.php" method="get">
 <input type="text" value="" name="id"/>
