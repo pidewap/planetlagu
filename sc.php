cb8a974649d0792e4fdbcc6c67fab4369054a095
pidewap <pidewap@gmail.com>
2017-10-14 12:53:50 +0700

    Update sc.php

--- sc.php
+++ sc.php
@@ -10,7 +10,22 @@ function bacaHTML($url){
   curl_close($data);
   return $hasil;
 }
+function copet($content,$start,$end){
+if($content && $start && $end) {
+$r = explode($start, $content);
+if (isset($r[1])){
+$r = explode($end, $r[1]);
+return $r[0];
+}return '';}}
+
 $kodeHTML =  bacaHTML('https://userscloud.com/go/n4du2itmzorv');
-$pecah = explode('<td class="strong" width="460"><a href="', $kodeHTML);
-$pecahLagi = explode('">', $pecah[1]);
+$pecah = explode('<td class="strong" width="460">', $kodeHTML);
+
+if(!empty($kodeHTML)){
+for($i=1; $i<=100; $i++){
+  $url = copet($pecah[$i],'href="//userscloud.com/','"');
+  echo 'http://userscloud.com/'.$url.'';
+}
+}
 ?>
+
