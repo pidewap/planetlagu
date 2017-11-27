96022c53305495a7829e7126bd8fea42a6806a6f
pidewap <pidewap@gmail.com>
2017-11-27 12:43:44 +0700

    Update lagu.php

--- lagu.php
+++ lagu.php
@@ -51,7 +51,7 @@ if(!empty($_GET['url'])){
   $linkdownload=str_replace('https://k2nblog.com/download/', '', $linkdownload);
   $linkdownload=str_replace('iTunes:', '<b>iTunes:</b>', $linkdownload);
   $linkdownload=str_replace('MP3:', '<b>MP3:</b>', $linkdownload);
-   $alink=explode('<a target="_blank" href="' , $linkdownload);
+   $alink=explode('<a' , $linkdownload);
    for ($i=0; $i<count($alink); $i++) {
   $alink[$i]=str_replace('http://linkshrink.net/zfb5=', '', $alinkk);
   $alink[$i]=str_replace('https://www.4shared.com', 'http://www.4shared.one', $alinkk);
